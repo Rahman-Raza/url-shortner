a90e05ae3668d816a6a2081613d14e8b495ce9cc
raraza <raziboy939@gmail.com>
2021-08-02 17:16:10 -0700

    models: encoder, decoder, Stats, Request/Results, Operations

---
+++ src/main/scala/org/raraza/models/package.scala
@@ -0,0 +1,23 @@
+package org.raraza
+
+import io.circe._
+
+package object models {
+
+  implicit val shortenRequestDecoder: Decoder[UrlShortenRequest] = Decoder.forProduct1("url")(UrlShortenRequest.apply)
+
+  implicit val statsEncoder: Encoder[Stats] = Encoder.forProduct1("callCount")(s => s.callCount)
+  implicit val statsDecoder: Decoder[Stats] = Decoder.forProduct1("callCount")(Stats.apply)
+
+  case class UrlShortenRequest(url: String)
+
+  case class Stats(callCount: Long)
+
+  object EntityOperations extends Enumeration {
+    type OperationStatus = Value
+    val EntityCreated, EntityFound, OperationFailed = Value
+  }
+
+  case class UrlShortenResult(code: String, status: EntityOperations.OperationStatus)
+
+}
